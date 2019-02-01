3c1849f43f53c4b2c1157c66095075f9afb4c57a
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-01-31 23:08:55 -0800

    [hashedpassword] using bcrypt

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '18426b92a85573844291a91f89f3e3e5'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
 
