0599279ace7912b4b2a101dc6be5a1e46c6b7bae
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-02-02 11:52:11 -0800

    user can update his account

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
@@ -43,3 +44,32 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
 
     submit = SubmitField('Login')
+
+
+class UpdatedAcocuntForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+
+    email = StringField('Email', validators=[DataRequired(), Email()])
+
+    submit = SubmitField('Update Account')
+
+    def validate_username(self,  username):
+        '''
+            Raise ValidationError if username exists in database alreay
+        '''
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError(
+                    'This username is taken! Please choose different one')
+
+    def validate_email(self,  email):
+        '''
+            Raise ValidationError if email exists in database alreay
+        ''' 
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError(
+                    'This email is taken! Please choose different one')
